1c512bca78dea903629b54ba99aa675518fb1b4c
stdrmwn <164896849+stdrmwn@users.noreply.github.com>
2024-05-12 21:25:18 +0700

    Add files via upload

---
+++ custom_bottom_bar.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import '../core/app_export.dart';
+
+enum BottomBarEnum { Home, Thrivehub, Account }
+// ignore_for_file: must_be_immutable
+
+// ignore_for_file: must_be_immutable
+class CustomBottomBar extends StatefulWidget {
+  CustomBottomBar({this.onChanged});
+
+  Function(BottomBarEnum)? onChanged;
+
+  @override
+  CustomBottomBarState createState() => CustomBottomBarState();
+}
+// ignore_for_file: must_be_immutable
+
+// ignore_for_file: must_be_immutable
+class CustomBottomBarState extends State<CustomBottomBar> {
+  int selectedIndex = 0;
+
+  List<BottomMenuModel> bottomMenuList = [
+    BottomMenuModel(
+      icon: ImageConstant.imgNavHome,
+      activeIcon: ImageConstant.imgNavHome,
+      title: "Home",
+      type: BottomBarEnum.Home,
+    ),
+    BottomMenuModel(
+      icon: ImageConstant.imgNavThrivehub,
+      activeIcon: ImageConstant.imgNavThrivehub,
+      title: "ThriveHub",
+      type: BottomBarEnum.Thrivehub,
+    ),
+    BottomMenuModel(
+      icon: ImageConstant.imgNavAccount,
+      activeIcon: ImageConstant.imgNavAccount,
+      title: "Account",
+      type: BottomBarEnum.Account,
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 88.v,
+      decoration: BoxDecoration(
+        color: appTheme.blue700,
+      ),
+      child: BottomNavigationBar(
+        backgroundColor: Colors.transparent,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedFontSize: 0,
+        elevation: 0,
+        currentIndex: selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        items: List.generate(bottomMenuList.length, (index) {
+          return BottomNavigationBarItem(
+            icon: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CustomImageView(
+                  imagePath: bottomMenuList[index].icon,
+                  height: 19.adaptSize,
+                  width: 19.adaptSize,
+                  color: appTheme.whiteA700,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 9.v),
+                  child: Text(
+                    bottomMenuList[index].title ?? "",
+                    style: theme.textTheme.bodyMedium!.copyWith(
+                      color: appTheme.whiteA700,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            activeIcon: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: AppDecoration.fillBlueGray.copyWith(
+                    borderRadius: BorderRadiusStyle.circleBorder16,
+                  ),
+                  child: CustomImageView(
+                    imagePath: bottomMenuList[index].activeIcon,
+                    height: 16.v,
+                    width: 18.h,
+                    color: appTheme.whiteA700,
+                    margin: EdgeInsets.fromLTRB(23.h, 7.v, 23.h, 8.v),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 4.v),
+                  child: Text(
+                    bottomMenuList[index].title ?? "",
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.bodyMedium!.copyWith(
+                      color: appTheme.whiteA700,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            label: '',
+          );
+        }),
+        onTap: (index) {
+          selectedIndex = index;
+          widget.onChanged?.call(bottomMenuList[index].type);
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
+// ignore_for_file: must_be_immutable
+
+// ignore_for_file: must_be_immutable
+class BottomMenuModel {
+  BottomMenuModel(
+      {required this.icon,
+      required this.activeIcon,
+      this.title,
+      required this.type});
+
+  String icon;
+
+  String activeIcon;
+
+  String? title;
+
+  BottomBarEnum type;
+}
+
+class DefaultWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xffffffff),
+      padding: EdgeInsets.all(10),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Please replace the respective Widget here',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
