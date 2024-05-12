06a0352818d0bb7cdbefcf060253cf4f486b1f3d
stdrmwn <164896849+stdrmwn@users.noreply.github.com>
2024-05-12 21:22:15 +0700

    Add files via upload

---
+++ image_constant.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: must_be_immutable
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+// Frame One images
+  static String imgSearchBlack90001 = '$imagePath/img_search_black_900_01.svg';
+
+  static String imgMenu = '$imagePath/img_menu.svg';
+
+  static String imgSignal = '$imagePath/img_signal.svg';
+
+  static String imgRectangle30 = '$imagePath/img_rectangle_30.png';
+
+  static String imgRectangle22 = '$imagePath/img_rectangle_22.png';
+
+  static String imgRectangle24 = '$imagePath/img_rectangle_24.png';
+
+  static String imgRectangle25 = '$imagePath/img_rectangle_25.png';
+
+  static String imgRectangle267 = '$imagePath/img_rectangle_267.png';
+
+// Frame Two images
+  static String imgEllipse9 = '$imagePath/img_ellipse_9.png';
+
+  static String imgEdit = '$imagePath/img_edit.svg';
+
+  static String imgFluentPeople = '$imagePath/img_fluent_people.svg';
+
+  static String imgArrowRight = '$imagePath/img_arrow_right.svg';
+
+  static String imgUserInterface = '$imagePath/img_user_interface.svg';
+
+  static String imgUser = '$imagePath/img_user.svg';
+
+  static String imgVector = '$imagePath/img_vector.svg';
+
+  static String imgSearch = '$imagePath/img_search.svg';
+
+  static String imgClock = '$imagePath/img_clock.svg';
+
+// Common images
+  static String imgNavHome = '$imagePath/img_nav_home.svg';
+
+  static String imgNavThrivehub = '$imagePath/img_nav_thrivehub.svg';
+
+  static String imgNavAccount = '$imagePath/img_nav_account.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
